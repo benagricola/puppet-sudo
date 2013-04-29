68de43f5290a34df8279d546f34b6ea233074262
Ben Agricola <bagricola@squiz.co.uk>
2013-04-29 22:19:00 +0100

    Fix indentation in sudo::from_hash
    Signed-off-by: Ben Agricola <bagricola@squiz.co.uk>

--- manifests/from_hash.pp
+++ manifests/from_hash.pp
@@ -5,6 +5,6 @@
 # hiera merge settings, this can merge directives from multiple
 # levels.
 class sudo::from_hash inherits sudo::params {
-	$directives = hiera_hash("%{calling_module}::directives",$default_directives)
+    $directives = hiera_hash("%{calling_module}::directives",$default_directives)
     create_resources('sudo::directive',$directive,$directive_defaults)
 }
